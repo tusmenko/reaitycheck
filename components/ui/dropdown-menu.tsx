4cc8d3ffa8b7c4d9518c7c2630a2f02f0c54c0ee
Yevhenii Tusmenko <tusmenkoe@gmail.com>
2026-03-06 14:40:41 +0100

    minor fixes

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -49,7 +49,7 @@ function DropdownMenuContent({
           "data-[side=right]:slide-in-from-left-2 " +
           "data-[side=top]:slide-in-from-bottom-2 z-50 " +
           "max-h-(--radix-dropdown-menu-content-available-height) " +
-          "min-w-[8rem] " +
+          "min-w-32 " +
           "origin-(--radix-dropdown-menu-content-transform-origin) " +
           "overflow-x-hidden overflow-y-auto rounded-md border p-1 shadow-md",
           className
@@ -88,11 +88,11 @@ function DropdownMenuItem({
         "data-[variant=destructive]:focus:bg-destructive/10 " +
         "dark:data-[variant=destructive]:focus:bg-destructive/20 " +
         "data-[variant=destructive]:focus:text-destructive " +
-        "data-[variant=destructive]:*:[svg]:!text-destructive " +
+        "data-[variant=destructive]:*:[svg]:text-destructive! " +
         "[&_svg:not([class*='text-'])]:text-muted-foreground relative flex " +
         "cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm " +
-        "outline-hidden select-none data-[disabled]:pointer-events-none " +
-        "data-[disabled]:opacity-50 data-[inset]:pl-8 " +
+        "outline-hidden select-none data-disabled:pointer-events-none " +
+        "data-disabled:opacity-50 data-inset:pl-8 " +
         "[&_svg]:pointer-events-none [&_svg]:shrink-0 " +
         "[&_svg:not([class*='size-'])]:size-4",
         className
@@ -114,8 +114,8 @@ function DropdownMenuCheckboxItem({
       className={cn(
         "focus:bg-accent focus:text-accent-foreground relative flex " +
         "cursor-default items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 " +
-        "text-sm outline-hidden select-none data-[disabled]:pointer-events-none " +
-        "data-[disabled]:opacity-50 [&_svg]:pointer-events-none " +
+        "text-sm outline-hidden select-none data-disabled:pointer-events-none " +
+        "data-disabled:opacity-50 [&_svg]:pointer-events-none " +
         "[&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
@@ -157,8 +157,8 @@ function DropdownMenuRadioItem({
       className={cn(
         "focus:bg-accent focus:text-accent-foreground relative flex " +
         "cursor-default items-center gap-2 rounded-sm py-1.5 pr-2 pl-8 " +
-        "text-sm outline-hidden select-none data-[disabled]:pointer-events-none " +
-        "data-[disabled]:opacity-50 [&_svg]:pointer-events-none " +
+        "text-sm outline-hidden select-none data-disabled:pointer-events-none " +
+        "data-disabled:opacity-50 [&_svg]:pointer-events-none " +
         "[&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
@@ -253,7 +253,7 @@ function DropdownMenuSubTrigger({
         "data-[state=open]:text-accent-foreground " +
         "[&_svg:not([class*='text-'])]:text-muted-foreground flex " +
         "cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm " +
-        "outline-hidden select-none data-[inset]:pl-8 " +
+        "outline-hidden select-none data-inset:pl-8 " +
         "[&_svg]:pointer-events-none [&_svg]:shrink-0 " +
         "[&_svg:not([class*='size-'])]:size-4",
         className
@@ -280,7 +280,7 @@ function DropdownMenuSubContent({
         "data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 " +
         "data-[side=left]:slide-in-from-right-2 " +
         "data-[side=right]:slide-in-from-left-2 " +
-        "data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] " +
+        "data-[side=top]:slide-in-from-bottom-2 z-50 min-w-32 " +
         "origin-(--radix-dropdown-menu-content-transform-origin) " +
         "overflow-hidden rounded-md border p-1 shadow-lg",
         className
